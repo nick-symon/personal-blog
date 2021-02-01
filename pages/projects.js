b45fbc729e7dcf3da5c66bd074d5c08c8ec044b7
nick-symon <symon.nick@gmail.com>
2021-02-01 17:58:26 -0600

    basic project page

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
 import NavHeader from '../components/NavHeader'
 import Head from 'next/head'
+import styles from '../styles/Projects.module.scss'
 
 export default function Projects(){
     return(
@@ -10,7 +11,14 @@ export default function Projects(){
         </Head>
 
         <NavHeader />
-        <h1>Check out mah projects!</h1>
+        <div className={styles.container}>
+            <h1 className={styles.mainHeader}>Projects</h1>
+            <a href="https://www.podpeople.app/" title="Pod People" target="_blank">
+                <h2>Pod People</h2>
+            </a>
+                <p>A social networking app centered around podcasts.</p>
+                <p>Built with React, Sass, and Django Rest Framework.</p>
+        </div>
         </>
     )
 } 
