5fa22c3d55fd5de518986c022b53e487956b056c
nick-symon <symon.nick@gmail.com>
2021-02-11 15:30:07 -0600

    navheader - add os click e listener

--- components/NavHeader.js
+++ components/NavHeader.js
@@ -1,10 +1,30 @@
 import Link from 'next/link'
 import styles from './navheader.module.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import cn from 'classnames' 
 
 export default function NavHeader() {
     const [active, setActive] = useState(false);
+
+    const handleOutsideClick = (e) => {
+        console.log(e.target.classList);
+        if (
+            active &&
+            !e.target.classList.contains(styles.link) &&
+            !e.target.classList.contains(styles.linkItem) &&
+            !e.target.classList.contains(styles.button) &&
+            !e.target.classList.contains(styles.burger)
+        ) {
+            setActive(!active)
+        }
+    }
+
+    useEffect(()=> {
+        document.addEventListener("mousedown", handleOutsideClick);
+        return () => {
+            document.addEventListener("mousedown", handleOutsideClick);
+        };
+    });
 
     return (
         <>
@@ -26,7 +46,7 @@ export default function NavHeader() {
                     >
                     <li className={styles.linkItem}><Link href="/projects"><a className={styles.link}>Projects</a></Link></li>
                     <li className={styles.linkItem}><Link href="/blog"><a className={styles.link}>Blog</a></Link></li>
-                    <li className={styles.linkItem}><Link href="/"><a className={styles.link}>About</a></Link></li>
+                    <li className={styles.linkItem}><Link href="/blog/about-me"><a className={styles.link}>About</a></Link></li>
                 </ul> 
         </div>
         </>       
